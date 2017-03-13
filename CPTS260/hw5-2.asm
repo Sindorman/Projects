425c3f5e2e928fd7517cd422cad3a87f1d9ea51a
Sindorman <mikebykhovtsev@gmail.com>
2017-03-13 13:50:02 -0700

    Add files via upload

---
+++ CPTS260/hw5-2.asm
@@ -0,0 +1,40 @@
+﻿# The function Θ(k), with k ≥ 2, as
+2^ + 4^2 + 6^2 + · · · + m^2
+#where m is the largest even number that is ≤ k. Please implement the
+#function in MIPS and make sure your implementation indeed works on the
+#simulator, by printing the value Θ(15) to the terminal.
+
+.text
+main:
+	li $a0, 15
+	jal f
+	add $a0, $v0, $zero
+	li $v0, 1
+	syscall
+	li $v0, 10
+	syscall
+	
+f:
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+	li $t1, 2	
+	move $t0, $a0
+	div $t0, $t1
+	mfhi $t0
+	beq $t0, 1, get
+	move $t0, $a0
+	j LOOP
+get:
+	move $t0, $a0
+	add $t0, $t0, -1
+	j LOOP	
+LOOP:
+	bgt $t1, $t0, EXIT
+	mul $t3, $t1, $t1
+	add $t1, $t1, 2
+	add $v0, $v0, $t3
+	j LOOP	
+EXIT:
+	lw $ra, 0($sp)
+	addiu $sp, $sp, 4
+	jr $ra
---
+++ CPTS260/hw5.asm
@@ -0,0 +1,43 @@
+﻿#Custom fibonachi function.
+#F(n) = 2 ∗ F(n − 1) + 3 ∗ F(n − 2);
+#F(0) = 1;
+#F(1) = 2
+.text
+main:
+	li $a0, 4
+	jal f
+	add $a0, $v0, $zero
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
+f:
+	bgt $a0, 1, case
+	beq $a0, 0, ret
+	li $v0, 2
+	jr $ra
+	
+case:
+	addiu $sp, $sp, -8
+	sw $a0, 4($sp)
+	sw $ra, 0($sp)
+	addi $a0, $a0, -1
+	jal f
+	addiu $sp, $sp, -4
+	sw $v0, 0($sp)
+	addi $a0, $a0, -1
+	jal f
+	lw $t0, 0($sp)	
+	mul $t0, $t0, 2
+	mul $v0, $v0, 3
+	add $v0, $t0, $v0
+	addiu $sp, $sp, 4
+	lw $ra, 0($sp)
+	lw $a0, 4($sp)
+	addiu $sp, $sp, 8
+	jr $ra
+
+ret:
+	li $v0, 1
+	jr $ra
